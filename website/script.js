d819c76ab0f85180326b8d546961e169a641f481
Dalmanski <jayraldjohndalman@gmail.com>
2024-12-19 11:14:34 +0800

    new file:   website/Sound effect/520579__divoljud__clickglass.wav 	new file:   website/Sound effect/624037__the_sample_workshop__2690_vial_tap_br-a-1-tmc.mp3 	new file:   website/Sound effect/Epic Sci-Fi Music_ Aphelion  Alibi Music.mp3 	modified:   website/index.html 	modified:   website/script.js 	modified:   website/styles.css

--- website/script.js
+++ website/script.js
@@ -6,26 +6,35 @@ const aiNameInput = document.getElementById("ai-name");
 const aiModeSelect = document.getElementById("ai-mode");
 const aiSelect = document.getElementById("ai-select");
 const typewriterSpeed = document.getElementById("typewriter-speed");
+const volumeIcon = document.getElementById("volume-icon");
 
 let keyName = "AI Database";
 let selectedAI = "AI Default";
 let AI_Database = JSON.parse(localStorage.getItem(keyName)) || {};
 if (!AI_Database.AI_Data) {
     AI_Database.AI_Data = {};
-    AI_Database.Settings = {Your_Name: "You", AI_Name: "AI", Typewriter_Speed: "0.02"};
+    AI_Database.Settings = {Your_Name: "You", AI_Name: "Baby AI", Typewriter_Speed: "0.02", Is_Muted: false};
 }
 let message = AI_Database.AI_Data[selectedAI] || [];
 
 let AI_MODE = "learning";  // learning or prompt
-let YOUR_NAME = AI_Database.Settings.Your_Name ?? "You";
-let AI_NAME = AI_Database.Settings.AI_Name ?? "AI";
-let setTypewriterSpeed = parseFloat(AI_Database.Settings.Typewriter_Speed) ?? 0.02;
+let YOUR_NAME = AI_Database.Settings.Your_Name;
+let AI_NAME = AI_Database.Settings.AI_Name;
+let setTypewriterSpeed = AI_Database.Settings.Typewriter_Speed;
+let isMuted = AI_Database.Settings.Is_Muted;
+if (isMuted) volumeIcon.className = "fa-solid fa-volume-xmark";
+else volumeIcon.className = "fa-solid fa-volume-high";
 
 localStorage.setItem(keyName, JSON.stringify(AI_Database));
 
 yourNameInput.value = YOUR_NAME;
 aiNameInput.value = AI_NAME;
 typewriterSpeed.value = setTypewriterSpeed;
+
+let isAudioAllowed = false;
+let bgMusic = new Audio("Sound effect\\Epic Sci-Fi Music_ Aphelion  Alibi Music.mp3");
+let userEnterSound = new Audio("Sound effect\\520579__divoljud__clickglass.wav");
+let soundBlip = new Audio("Sound effect\\624037__the_sample_workshop__2690_vial_tap_br-a-1-tmc.mp3");
 
 function sequenceMatcher(inputText, storedText, threshold = 0.75) {
     let similarity = 0, m = 0, n = 0;
@@ -74,6 +83,55 @@ async function delay(time) {
     await new Promise(resolve => setTimeout(resolve, time));
 }
 
+// To start music and sound (because it doesn't work on autoplay due to security reasons)
+let isMusicPlaying = false;
+document.addEventListener('click', () => {
+    if (!isMuted && !isMusicPlaying) {
+        bgMusic.loop = true;
+        bgMusic.volume = 0;
+        function fadeInAudio(audio, duration, targetVolume) {
+            const step = 0.01;
+            const interval = (duration * 1000) / (targetVolume / step);
+            let currentVolume = audio.volume;
+            return new Promise((resolve) => {
+                const fadeInterval = setInterval(() => {
+                    currentVolume = Math.min(currentVolume + step, targetVolume);
+                    audio.volume = currentVolume;
+                    if (currentVolume >= targetVolume) {
+                        clearInterval(fadeInterval);
+                        resolve();
+                    }
+                }, interval);
+            });
+        }
+        bgMusic.play()
+            .then(() => {
+                isMusicPlaying = true;
+                return fadeInAudio(bgMusic, 3, 0.5);
+            })
+    }
+    if (!isAudioAllowed) {
+        isAudioAllowed = true;
+    }
+});
+
+function toggleVolume() {
+    if (isAudioAllowed) {    
+        if (isMuted) {
+            bgMusic.volume = 0.5;
+            soundBlip.volume = 1;
+            volumeIcon.className = "fa-solid fa-volume-high";
+        } else {
+            bgMusic.volume = 0; 
+            soundBlip.volume = 0;
+            volumeIcon.className = "fa-solid fa-volume-xmark";
+        }
+        isMuted = !isMuted;
+        AI_Database.Settings.Is_Muted = isMuted;
+        localStorage.setItem(keyName, JSON.stringify(AI_Database)); 
+    } 
+}
+
 async function aiResponse(response) {
     toggleAutoScroll(true);
     userInput.value = '';
@@ -82,6 +140,10 @@ async function aiResponse(response) {
     await delay(0.5);
     for (let i = 0; i < response.length; i++) {
         chatArea.value += response[i];
+        if (isAudioAllowed) {
+            soundBlip.currentTime = 0;
+            soundBlip.play();
+        }
         await delay(setTypewriterSpeed);
     }
     await delay(0.2);
@@ -90,6 +152,10 @@ async function aiResponse(response) {
 
 async function yourResponse(response){
     toggleAutoScroll(true);
+    if (isAudioAllowed) {
+        userEnterSound.currentTime = 0;
+        userEnterSound.play();
+    }
     chatArea.value += `\n${YOUR_NAME}: ${response.capitalize()}`;
     await delay(0.1);
     toggleAutoScroll(false);
@@ -242,6 +308,7 @@ async function handleUserInput() {
                 } // else, it will proceed to I dont understand that
 
                 // this part will pass when there's no declare on "continue;"
+                // AI is going to .perspShift() but your not .perspShift()
                 await aiResponse(`How to respond "${yourChat.perspShift()}"?`);
                 const AiChat = await getUserEnter(`What should the AI learn from "${yourChat.perspShift()}"? Or type [nevermind, nvm].`);
                 if (AiChat.includes("nevermind") || AiChat.includes("nvm")) {
@@ -261,30 +328,25 @@ async function handleUserInput() {
     } 
 }
 
-yourNameInput.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter' && !e.shiftKey) {
-        YOUR_NAME = yourNameInput.value;
-        AI_Database.Settings.Your_Name = YOUR_NAME;
-        localStorage.setItem(keyName, JSON.stringify(AI_Database));  
-        yourResponse(`(You change your name to "${YOUR_NAME}")`);
-    }
+yourNameInput.addEventListener('blur', () => {
+    YOUR_NAME = yourNameInput.value;
+    AI_Database.Settings.Your_Name = YOUR_NAME;
+    localStorage.setItem(keyName, JSON.stringify(AI_Database));  
+    yourResponse(`(You change your name to "${YOUR_NAME}")`);
 });
 
-aiNameInput.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter' && !e.shiftKey) {
-        AI_NAME = aiNameInput.value;
-        AI_Database.Settings.AI_Name = AI_NAME;
-        localStorage.setItem(keyName, JSON.stringify(AI_Database));  
-        yourResponse(`(You change the AI name to "${AI_NAME}")`);
-    }
+aiNameInput.addEventListener('blur', () => {
+    AI_NAME = aiNameInput.value;
+    AI_Database.Settings.AI_Name = AI_NAME;
+    localStorage.setItem(keyName, JSON.stringify(AI_Database));  
+    yourResponse(`(You changed the AI name to "${AI_NAME}")`);
 });
 
-typewriterSpeed.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter' && !e.shiftKey) {
-        setTypewriterSpeed = typewriterSpeed.value;
-        AI_Database.Settings.Typewriter_Speed = setTypewriterSpeed;
-        localStorage.setItem(keyName, JSON.stringify(AI_Database));  
-    }
+typewriterSpeed.addEventListener('blur', () => {
+    setTypewriterSpeed = typewriterSpeed.value;
+    AI_Database.Settings.Typewriter_Speed = setTypewriterSpeed;
+    localStorage.setItem(keyName, JSON.stringify(AI_Database));  
+    alert("Typewriter speed changed successfully!");
 });
 
 aiModeSelect.addEventListener('change', function (event) {
@@ -317,7 +379,7 @@ function showSection(sectionId) {
 function loadLocalStorage() {
     const preloadMessage = [
         ["how are you?", "i am fine"],
-        ["whats your name?", "you are powerful ai"],
+        ["whats your name?", "I am powerful ai"],
         ["whats my name?", "my name is nobody"],
         ["eeeyy", "nice"],
         ["is ai replace job", "yes"],
@@ -351,7 +413,7 @@ function loadLocalStorage() {
     const userConfirmed = confirm('Are you sure you want to add "Load AI sample" with data?');
     if (userConfirmed) {
         AI_Database.AI_Data["Load AI sample"] = preloadMessage;
-        AI_Database.Settings = {Your_Name: "You", AI_Name: "AI", Typewriter_Speed: "0.02"};
+        AI_Database.Settings = {Your_Name: "You", AI_Name: "Baby AI", Typewriter_Speed: "0.02", Is_Muted: false};
         localStorage.setItem(keyName, JSON.stringify(AI_Database));
         alert('Done loading sample data.');
         message = preloadMessage;
@@ -374,6 +436,20 @@ function deleteAiData() {
         }
     } else {
         alert("You cannot delete when only one AI is selected.");
+    }
+}
+
+function deleteAiDatabase() {
+    const confirmed = confirm(`Are you sure you want to delete the key "${keyName}" from localStorage? This can't be undone!`);
+    if (confirmed) {
+        if (localStorage.getItem(keyName) !== null) {
+            localStorage.removeItem(keyName);
+            alert(`"${keyName}" has been deleted from localStorage.`);
+            aiSelectOption();
+            displayStorageSize();
+        } else {
+            alert(`The key "${keyName}" does not exist in localStorage.`);
+        }
     }
 }
 
@@ -434,7 +510,7 @@ function aiSelectOption() { // Also, auto produce "AI Default" if empty
     }
 }
 
-document.getElementById("ai-select").addEventListener("change", (event) => {
+aiSelect.addEventListener("change", (event) => {
     selectedAI = event.target.value;
     message = AI_Database.AI_Data[selectedAI] || [];
     yourResponse(`(You switched to "${selectedAI}")`);
